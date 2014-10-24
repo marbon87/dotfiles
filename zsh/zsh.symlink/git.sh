3cc1183c6f246dc88831c954567e0f86e6f77659
Markus Boese <mboese@wolterskluwer.de>
2014-10-24 17:04:36 +0200

    add git branch -a

--- zsh/zsh.symlink/git.sh
+++ zsh/zsh.symlink/git.sh
@@ -8,4 +8,4 @@ alias gp='git push'
 alias gpb='git push --set-upstream origin HEAD' # Push local branch to remote
 alias gco='git checkout'
 alias changelog='git log `git log -1 --format=%H -- CHANGELOG*`..; cat CHANGELOG*'
-
+alias gb='git branch -a'
