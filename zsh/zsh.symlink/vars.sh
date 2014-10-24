bd86279c4ac40527a44da735543fc1f6f0087fe4
Markus Boese <mboese@wolterskluwer.de>
2014-10-24 14:03:10 +0200

    locale set to de_DE.UTF-8.

--- zsh/zsh.symlink/vars.sh
+++ zsh/zsh.symlink/vars.sh
@@ -1,5 +1,14 @@
 # general setting
 
+# locale
+export LANG="de_DE.UTF-8"
+export LC_COLLATE="de_DE.UTF-8"
+export LC_CTYPE="de_DE.UTF-8"
+export LC_MESSAGES="de_DE.UTF-8"
+export LC_MONETARY="de_DE.UTF-8"
+export LC_NUMERIC="de_DE.UTF-8"
+export LC_TIME="de_DE.UTF-8"
+
 #cli
 export SCRIPTS=$HOME/bin/scripts
 
