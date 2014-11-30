c82bd3b30971118c239e33dc39528723c0eb593d
Mark Bonnekessel <bonnekessel@mailbox.org>
2014-11-30 09:57:44 +0100

    cleanup

--- homebrew/config.bash
+++
@@ -1,2 +0,0 @@
-# No beer for the Terminal.
-export HOMEBREW_NO_EMOJI=1
--- homebrew/path.bash
+++
@@ -1,3 +0,0 @@
-# Prepend homebrew's sbin directory to PATH.
-export PATH=/usr/local/sbin:$PATH
-export PATH=/usr/local/bin:$PATH
