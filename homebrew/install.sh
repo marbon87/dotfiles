2b73309b91661f1e216d66a4ac0b09e341dd3e20
Mark Bonnekessel <bonnekessel@mailbox.org>
2014-12-12 17:50:08 +0100

    homebrew only on mac

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
-if test ! $(which brew); then
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-    brew tap homebrew/science
-    brew install wget
-    brew install git
-    brew install python
-    brew install zsh
+if [ "$(uname -s)" == "Darwin" ]; then
+    if test ! $(which brew); then
+        ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+        brew tap homebrew/science
+        brew install wget
+        brew install git
+        brew install python
+        brew install zsh
+    fi
 fi
+
