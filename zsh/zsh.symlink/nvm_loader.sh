a36cd46452ab8fe4806953ef5eaacb234cf2fd19
Mark Bonnekessel <m.bonnekessel@lvm.de>
2018-02-22 14:23:28 +0100

    fix

--- zsh/zsh.symlink/nvm_loader.sh
+++ zsh/zsh.symlink/nvm_loader.sh
@@ -4,7 +4,7 @@
 if [ -s "$HOME/.nvm/nvm.sh" ] && [ ! "$(whence -w __init_nvm)" = function ]; then
   export NVM_DIR="/home/${USER}/.nvm"
   [ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"
-  declare -a __node_commands=('nvm' 'node' 'npm' 'yarn' 'gulp' 'grunt' 'webpack', 'ng')
+  declare -a __node_commands=('nvm' 'node' 'npm' 'yarn' 'gulp' 'grunt' 'webpack' 'ng')
   function __init_nvm() {
     for i in "${__node_commands[@]}"; do unalias $i; done
     . "$NVM_DIR"/nvm.sh
